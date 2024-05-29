28594a93cc031e9d75b2858324e3814e22e76bc2
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 11:56:25 +0530

    Added: `discord-moderation-bot/src/features/auto-mute.js`

---
+++ discord-moderation-bot/src/features/auto-mute.js
@@ -0,0 +1,13 @@
+```js
+// auto-mute.js
+
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'auto-mute',
+  description: 'Automatically mutes users who spam or use inappropriate language',
+  execute(message, args) {
+    // Logic for automatically muting users based on defined criteria
+  }
+};
+```
